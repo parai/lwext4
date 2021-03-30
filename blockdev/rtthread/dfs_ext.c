2543c1189305b52f2e618ae8898de12c92affdba
Meco Man <920369182@qq.com>
2021-03-30 22:42:06 +0800

    fix bug https://github.com/RT-Thread/rt-thread/pull/4530

--- blockdev/rtthread/dfs_ext.c
+++ blockdev/rtthread/dfs_ext.c
@@ -1,21 +1,7 @@
 /*
- * File      : dfs_ext.c
- * This file is part of Device File System in RT-Thread RTOS
- * COPYRIGHT (C) 2004-2021, RT-Thread Development Team
+ * Copyright (c) 2006-2021, RT-Thread Development Team
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author            Notes
@@ -392,7 +378,7 @@ static int dfs_ext_getdents(struct dfs_fd* file, struct dirent* dirp, rt_uint32_
         rentry = ext4_dir_entry_next(file->data);
         if(NULL != rentry)
         {
-            strncpy(d->d_name, rentry->name, sizeof(d->d_name));
+            strncpy(d->d_name, rentry->name, DFS_PATH_MAX);
             if(EXT4_DE_DIR == rentry->inode_type)
             {
                 d->d_type = DT_DIR;
